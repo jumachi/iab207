53bcd929b4e6f9281b8ccc5e5a4df499a79e02de
jumachi <julianmchitty@gmail.com>
2025-05-27 15:12:34 +1000

    Update models.py

--- week11_api/travel/models.py
+++ week11_api/travel/models.py
@@ -35,7 +35,7 @@ class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(400))
-    created_at = db.Column(db.DateTime, default=datetime.now())
+    created_at = db.Column(db.DateTime, default=datetime.now.strftime())
     # add the foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
